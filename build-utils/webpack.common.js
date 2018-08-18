36c27d8dba209f2b783b0cffee7096500b18f3e3
azhry <arliansyah_azhary@yahoo.com>
2018-08-18 15:03:22 +0700

    first commit

---
+++ build-utils/webpack.common.js
@@ -0,0 +1,46 @@
+const commonPaths = require('./common-paths');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const config = {
+	entry: {
+		vendor: ['semantic-ui-react']
+	},
+	output: {
+		path: commonPaths.outputPath,
+		publicPath: '/'
+	},
+	module: {
+		rules: [
+		  	{
+			    test: /\.(js)$/,
+			    exclude: /node_modules/,
+			    use: ['babel-loader']
+		  	}
+		]
+	},
+	optimization: {
+		splitChunks: {
+		  	cacheGroups: {
+			    vendor: {
+					chunks: 'initial',
+					test: 'vendor',
+					name: 'vendor',
+					enforce: true
+			    }
+		  	}
+		}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'public/index.html',
+			favicon: 'public/favicon.png',
+			minify: {
+				collapseWhitespace: true,
+				collapseInlineTagWhitespace: true,
+				removeComments: true,
+				removeRedundantAttributes: true
+		    }
+		})
+	]
+};
+module.exports = config;
